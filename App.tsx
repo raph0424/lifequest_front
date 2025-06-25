4e59fcdbeed9f30b30af911a1a41b7534d0b5a3e
unknown <raphael0424@gmail.com>
2025-06-25 19:17:18 +0200

    first commit

---
+++ App.tsx
@@ -0,0 +1,19 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './screens/HomeScreen';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        {/* ajoute d'autres écrans ici */}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ screens/HomeScreen.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-red-500 text-xl font-bold">
+        Hello NativeWind 🚀
+      </Text>
+    </View>
+  );
+}
